336dadd9b018d205d02f41f07afcf89b813f9eac
Samuel Dahlborg <beaosam@gmail.com>
2024-03-23 00:04:09 +0100

    player now melts

---
+++ Melting_Rush/Assets/Player/Melting.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Melting : MonoBehaviour
+{
+    [SerializeField] Transform playerGraphics;
+    [SerializeField] GameObject playerPhysics;
+    private Transform pPhysicsTransform;
+    private Rigidbody2D pRigidBody;
+
+    private float startMass;
+
+    private float time = 0;
+    [SerializeField] private float minScale;
+    [SerializeField] private float startScale;
+    [SerializeField] private float maxTime;
+    private void Start() {
+        pPhysicsTransform = playerPhysics.GetComponentInChildren<Transform>();
+        pRigidBody = playerPhysics.GetComponentInChildren<Rigidbody2D>();
+        startMass = pRigidBody.mass;
+
+        Vector3 startVector = new Vector3(startScale, startScale, startScale);
+        playerGraphics.localScale = startVector;
+        pPhysicsTransform.localScale = startVector;
+
+        time = 0;
+        
+    }
+    private void Update() {
+        time += Time.deltaTime;
+        float timeLeft = maxTime - time;
+        float procentOfMax = timeLeft/maxTime;
+        float currentScale = (startScale-minScale)*procentOfMax + minScale;
+        if(currentScale > minScale) {
+            Vector3 scaleVector = new Vector3(currentScale, currentScale, currentScale);
+            playerGraphics.localScale = scaleVector;
+            pPhysicsTransform.localScale = scaleVector;
+            pRigidBody.mass = startMass*(1+time/maxTime);
+        }
+    }
+}
