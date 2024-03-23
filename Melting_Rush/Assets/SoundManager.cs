0b9e0f3eda5a3d5a89b63658c9f091246929858f
Samuel Dahlborg <beaosam@gmail.com>
2024-03-23 15:03:38 +0100

    tweaked sound manager

--- Melting_Rush/Assets/SoundManager.cs
+++ Melting_Rush/Assets/SoundManager.cs
@@ -1,9 +1,5 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
@@ -17,38 +13,70 @@ public class SoundManager : MonoBehaviour {
         Chiptune
     }
     [SerializeField] SoundAudioClip[] soundAudioClipArray;
+    [SerializeField] GameObject audioSourcePrefab;
+    private Stack<AudioSource> disabledSources = new Stack<AudioSource>();
 
     private void Awake() {
+        
+    }
+
+    private void Start() {
         if(instance != null) {
             Debug.LogWarning("More than one sound manager!");
         } else {
             instance = this;
         }
-    }
-
-    private void Start() {
         PlaySound(Sound.Chiptune);
     }
 
     public void PlaySound(Sound sound) {
-        GameObject soundObject = new GameObject("Sound");
-        AudioSource source = soundObject.AddComponent<AudioSource>();
-        source.PlayOneShot(GetAudioClip(sound));
+        AudioSource source = GetSource();
+        SoundAudioClip soundAudioClip = GetAudioClip(sound);
+        source.clip = soundAudioClip.audioClip;
+        source.volume = soundAudioClip.volume;
+        source.Play();
+        StartCoroutine(WaitThenDisable(source, source.clip.length));
     }
-    private AudioClip GetAudioClip(Sound sound) {
+    private SoundAudioClip GetAudioClip(Sound sound) {
         foreach(SoundAudioClip soundAudioClip in soundAudioClipArray) {
             if(soundAudioClip.sound == sound) {
-                return soundAudioClip.audioClip;
+                return soundAudioClip;
             }
         }
         Debug.LogError("Sound: " +sound+" does not exist");
         return null;
     }
+
+    private void DisableSource(AudioSource source){
+        source.gameObject.SetActive(false);
+        disabledSources.Push(source);
+    }
+
+    private AudioSource GetSource() {
+        AudioSource source;
+        if(disabledSources.Count == 0) {
+            GameObject obj = Instantiate(audioSourcePrefab, transform);
+            source = obj.GetComponent<AudioSource>();
+        } else {
+            source = disabledSources.Pop();
+        }
+        source.gameObject.SetActive(true);
+        return source;
+    }
+
+    private IEnumerator WaitThenDisable(AudioSource source, float t) {
+        yield return new WaitForSeconds(t);
+        DisableSource(source);
+    }
+
 }
 
 [System.Serializable] public class SoundAudioClip {
     public SoundManager.Sound sound;
     public AudioClip audioClip;
+    
+    [Range(0.0f, 1.0f)]
+    public float volume;
 }
 
 
