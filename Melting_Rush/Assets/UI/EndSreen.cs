f670f36f84579ee633fcf8a8ea1e77667fc21e84
Samuel Dahlborg <beaosam@gmail.com>
2024-03-24 14:17:39 +0100

    Added Endscreen UI

---
+++ Melting_Rush/Assets/UI/EndSreen.cs
@@ -0,0 +1,47 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSreen : MonoBehaviour
+{
+    [SerializeField] GameObject container;
+    [SerializeField] GameObject escaped;
+    [SerializeField] TextMeshProUGUI iceLeft;
+    [SerializeField] TextMeshProUGUI timeTaken;
+    [SerializeField] GameObject buttons;
+    // Start is called before the first frame update
+    void Start()
+    {
+        container.SetActive(false);
+        EndPoint.instance.onEndLevel += LevelComplete;
+        LeanTween.moveLocalX(escaped, -1600, 0f).setIgnoreTimeScale(true);
+        LeanTween.moveLocalX(iceLeft.gameObject, -1700, 0f).setIgnoreTimeScale(true);
+        LeanTween.moveLocalX(timeTaken.gameObject, -1700, 0f).setIgnoreTimeScale(true);
+        LeanTween.moveLocalX(buttons, -1600, 0f).setIgnoreTimeScale(true);
+    }
+
+    private void LevelComplete() {
+        Melting melting = FindObjectOfType<Melting>();
+        container.SetActive(true);
+
+        LeanTween.moveLocalX(escaped, 0, 1f).setEase(LeanTweenType.easeOutBack).setIgnoreTimeScale(true);
+
+        iceLeft.text = (melting.GetIceLeft()*100).ToString("F0") + "% Ice left!";
+        LeanTween.moveLocalX(iceLeft.gameObject, 0, 1f).setDelay(0.1f).setEase(LeanTweenType.easeOutBack).setIgnoreTimeScale(true);
+
+        timeTaken.text = "Time: " + melting.GetTime().ToString("F2");
+        LeanTween.moveLocalX(timeTaken.gameObject, 0, 1f).setDelay(0.1f).setEase(LeanTweenType.easeOutBack).setIgnoreTimeScale(true);
+
+        LeanTween.moveLocalX(buttons, 0, 1f).setDelay(0.2f).setEase(LeanTweenType.easeOutBack).setIgnoreTimeScale(true);
+    }
+
+    public void LoadMainMenu() {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Exit() {
+        Time.timeScale = 1;
+        Application.Quit();
+    }
+}
